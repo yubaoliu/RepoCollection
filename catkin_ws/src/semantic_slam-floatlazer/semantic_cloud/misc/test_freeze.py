d5c89e4c8c04e2a9ffd942916a22fbac15ae3f7c
yubao <yubaoliu89@gmail.com>
2019-05-31 16:00:05 +0900

    fix repo config

--- catkin_ws/src/semantic_slam-floatlazer/semantic_cloud/misc/test_freeze.py
+++
@@ -1,9 +0,0 @@
-from ptsemseg.models import get_model
-
-def freeze(m):
-    if m.__class__.__name__ == 'BatchNorm2d':
-        print m
-
-psp = get_model('pspnet', 38, version = 'sunrgbd')
-#print(psp.cbr_final.cbr_unit)
-psp.apply(freeze)
